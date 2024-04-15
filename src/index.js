5f67fd8836c3b78730aad368ea0e67cae7f09aa2
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-04-15 14:35:38 -0600

    bable

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import './functionality/todo.js'
-import './base.css'
+import './Index.css'
+const test = document.querySelector('input');
+
+const validation = document.querySelector('#button');
+const constraints ={
+    email:[
+        '[aA-zZ]+@+[aA-zZ]+.+[a-z]{3}',
+        'The email you have input is invalid. x@x.xxx is the correct form',
+    ],
+    country:[],
+    zip:[],
+    password:[],
+    passwordconfirm:[],
+}
+regex1 = new RegExp(constraints.email[0])
+validation.addEventListener('click',(e)=>{
+    document.querySelectorAll('input').forEach(element =>{
+        input = element.value
+        if(input == ''){
+            each = `${element.id}`
+        }
+    })
+    e.preventDefault()
+})
+
--- src/tester/test.js
+++
@@ -1,4 +0,0 @@
-import './test.css'
-import '../functionality/todo.js'
-import { dialogbox } from '../functionality/todo.js'
-dialogbox()
